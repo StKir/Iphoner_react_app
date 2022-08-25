a666757e755546e1b53a622c00ff977c0050dbe2
Strukov Kirill <kirillstrukov2@gmail.com>
2022-08-25 16:12:54 +0300

    first1

---
+++ src/components/app/App.js
@@ -0,0 +1,12 @@
+import AppHeader from "../appHeader/AppHeader";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+        <AppHeader/>
+    </div>
+  );
+}
+
+export default App;
