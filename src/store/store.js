bfe4410e1812637d26d9a472f994512e9ae12d70
Kirill <kirillstrukov2@gmail.com>
2022-10-19 17:52:36 +0300

    basket save local with redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,45 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import iphons from './iphonsSlice';
 import filters from './filtersSlice';
 import basket from './basketSlice';
 
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+	key: 'root',
+	storage,
+	whitelist: ['basket']
+};
+
+const rootRedusers = combineReducers({
+	iphons,
+	filters,
+	basket
+});
+
+const persistedReducer = persistReducer(persistConfig, rootRedusers);
+
 const store = configureStore({
-	reducer: { iphons, filters, basket },
-	middleware: (getDefaultMiddleware) => getDefaultMiddleware(), // Redux-thunk встроен в стандартные мидлвееры!
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+			}
+		}), // Redux-thunk встроен в стандартные мидлвееры!
 	devTools: process.env.NODE_ENV !== 'production'
 });
+
+export const persistor = persistStore(store);
 
 export default store;
